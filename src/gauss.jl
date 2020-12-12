add1aaf6079567f5fbc5aa20e2840f3fa6b1a9a4
Nikola Stoyanov <nstoyanov.92@gmail.com>
2020-12-12 23:06:03 +0000

    Better gaussian

--- src/gauss.jl
+++ src/gauss.jl
@@ -1,6 +1,6 @@
 module gauss
 
-export naive_gauss
+export naive_gauss, better_gauss
 
 function naive_gauss(A)
     n = size(A)[1]
@@ -15,6 +15,34 @@ function naive_gauss(A)
     end
 
     return U
+end
+
+function better_gauss(A::AbstractMatrix)
+    n = size(A, 1)
+    pivot = A[1, 1]
+    U = copy!(similar(A, eltype(pivot)), A)
+
+    r = 1
+    for j in 1:n
+        mi = argmax(abs.(U[r:end, j])) + r - 1
+        swap_rows!(U, mi, r)
+
+        for i in r+1:n
+            pivot = -U[i, j] / U[j, j]
+            U[i, :] = U[i, :] + U[j, :] * pivot
+        end
+        r += 1
+    end
+
+    return U
+end
+
+function swap_rows!(A::AbstractMatrix, i::Integer, j::Integer)
+    for k in axes(A, 2)
+        t = A[i, k]
+        A[i, k] = A[j, k]
+        A[j, k] = t
+    end
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,14 @@
 using gauss
 using Test
 
-@testset "Naive gaussian elimination works" begin
-    A = [2.0 1.0 -1.0; -3.0 -1.0 2.0; -2.0 1.0 2.0]
-    U = [2.0 1.0 -1.0; 0.0 0.5 0.5; 0.0 0.0 -1.0]
+A = [2.0 1.0 -1.0; -3.0 -1.0 2.0; -2.0 1.0 2.0]
 
+@testset "Naive gaussian elimination works" begin
+    U = [2.0 1.0 -1.0; 0.0 0.5 0.5; 0.0 0.0 -1.0]
     @test naive_gauss(A) ≈ U
+end
+
+@testset "Better gaussian elimination works" begin
+    U = [-3.0  -1.0 2.0; 0.0 1.66667 0.666667; 0.0 0.0 0.2]
+    @test isapprox(better_gauss(A), U, atol = 1e-5)
 end
